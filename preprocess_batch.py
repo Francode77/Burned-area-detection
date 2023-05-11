7bbd23037330ca6249d71c430ccdc4abd60b326e
Francode77 <francodefall@gmail.com>
2023-05-11 14:50:27 +0200

    incl extra data

--- preprocess_batch.py
+++ preprocess_batch.py
@@ -1,19 +1,30 @@
 from field import Field
 import os
 
-# Fold 0 : 78
-# Fold 1 : 55
-# Fold 2 : 69
-# Fold 3 : 85
-# Fold 4 : 69
-files=['train_eval.hdf5'
-#       ,'california_0.hdf5','california_1.hdf5','california_2.hdf5'
-       ]
-fold_len=[78,55,69,85,69]
-for file in files:
-    source_file=os.path.join('data',file)
-    for fold in range (1,5):
-        for x in range (0,fold_len[fold]):
-            print (fold, x)
+# 'train_eval.hdf5'
+source_file=os.path.join('data','train_eval.hdf5')
+for fold in range (1,5):
+    for x in range (0,99):
+        print (fold, x)
+        try: 
             see=Field(source_file,fold,x)
             see.write_metric() 
+        except:
+            break
+
+# Extra data  
+fold_lengths=[6,6,1]
+files=['california_0.hdf5','california_1.hdf5','california_2.hdf5']
+for idx, file in enumerate(files):
+    source_file=os.path.join('data',file)
+    for fold in range (0,7):
+        try: 
+            for x in range (0,99):
+                print (file, fold, x)
+                try:
+                    see=Field(source_file,fold,x)
+                    see.write_metric() 
+                except:
+                    break
+        except:
+            break
