fe304f007fdf554c8df20b39a56e0c39bf72033b
Francode77 <francodefall@gmail.com>
2023-05-13 23:04:30 +0200

    update

--- verify_predictions.py
+++ verify_predictions.py
@@ -15,7 +15,9 @@ for fold in range (1):
             image = DataSource.get_image(source_file,fold, x, 0) 
             mask_truth = DataSource.get_mask(source_file, fold, x)
             metric, prediction, mask_pred = MakePrediction.predict(image)  
-            FieldPlotter.plot_evaluation(image, metric, prediction, mask_truth, 2)    
+            FieldPlotter.plot_img(self, image, 0)
+            FieldPlotter.plot_evaluation(image, metric, prediction, mask_truth, 2) 
+            input("Press Enter to continue...")   
             
         except IndexError:
             break
